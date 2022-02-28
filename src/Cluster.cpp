dd867c2525f0d496e30f3a6e30f4b167119df4ba
Jóhannes Nordal <johannesnordal88@gmail.com>
2022-02-28 23:22:18 +0000

    add: cluster

--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -2,24 +2,42 @@
 #include "khash.h"
 #include "kvec.h"
 
-#include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <vector>
 
-uint32_t k;
-uint32_t c;
-uint32_t s;
-uint64_t lim = 990;
-bool validate = false;
-
-struct UnionFind
+// khash.h doesn't play well with std::vector<T> and needs a pointer
+// to a heap allocated std::vector<T> so it doesn't segfault, adding unnecessary
+// indirection and memory allocation. kvec.h offers a fast vector
+// implemented using macros. It creates an anonymous struct which
+// needs to be wrapped into a named struct so it can be
+// referred to elsewhere, hence the Kvector wrapper.
+struct Kvector
 {
+    kvec_t(uint64_t) vec;
+};
+
+KHASH_MAP_INIT_INT64(kvector, Kvector);
+KHASH_MAP_INIT_INT64(u64, uint64_t);
+
+using ClusterHashTable = khash_t(kvector)*;
+using HashLocator = khash_t(kvector)*;
+using MinHash = std::vector<uint64_t>;
+using MinHashList = std::vector<MinHash>;
+using SketchFileNames = std::vector<std::string>;
+
+struct Clusters
+{
+    
+    ClusterHashTable ctable = nullptr;
     int *p;
     int *r;
     int size;
 
-    UnionFind(int n) : size{n}
+    static uint64_t limit;
+
+    Clusters(int n) : size{n}
     {
         p = (int*) malloc(sizeof(int) * size);
         r = (int*) malloc(sizeof(int) * size);
@@ -40,7 +58,7 @@ struct UnionFind
         return x;
     }
 
-    void merge(const int x, const int y)
+    void join(const int x, const int y)
     {
         int xm = find(x);
         int ym = find(y);
@@ -59,45 +77,94 @@ struct UnionFind
         }
     }
 
-    ~UnionFind()
+    void build_table()
+    {
+        if (ctable != nullptr)
+        {
+            kh_destroy(kvector, ctable);
+            ctable = kh_init(kvector);
+        }
+
+        int ret;
+        khiter_t k;
+
+        for (int x = 0; x < size; x++)
+        {
+            const int parent = find(x);
+
+            if (k == kh_end(ctable))
+            {
+                k = kh_put(kvector, ctable, parent, &ret);
+                Kvector v;
+                kv_init(v.vec);
+                kh_value(ctable, k) = v;
+            }
+
+            kv_push(uint64_t, kh_value(ctable, k).vec, x);
+        }
+    }
+
+    ~Clusters()
     {
         free(p);
         free(r);
+        kh_destroy(kvector, ctable);
     }
 };
 
-struct Kvec
-{
-    kvec_t(uint64_t) kvec_array;
-};
-
-KHASH_MAP_INIT_INT64(list, Kvec);
-
-khash_t(list) *make_hash_locator(std::vector<uint64_t*>& sketches)
+HashLocator locate_hashes(MinHashList& min_hash_list)
 {
     int ret;
     khiter_t k;
-    khash_t(list) *hash_locator = kh_init(list);
+    HashLocator hash_locator = kh_init(kvector);
 
-    for (int i = 0; i < sketches.size(); i++)
+    for (int i = 0; i < min_hash_list.size(); i++)
     {
-        for (int j = 0; j < s; j++)
+        for (auto hash : min_hash_list[i])
         {
-            k = kh_get(list, hash_locator, sketches[i][j]);
+            k = kh_get(kvector, hash_locator, hash);
 
             if (k == kh_end(hash_locator))
             {
-                k = kh_put(list, hash_locator, sketches[i][j], &ret);
-                Kvec v;
-                kv_init(v.kvec_array);
-                kh_value(hash_locator, k) = v;
+                k = kh_put(kvector, hash_locator, hash, &ret);
+                Kvector kv;
+                kv_init(kv.vec);
+                kh_value(hash_locator, k) = kv;
             }
 
-            kv_push(uint64_t, kh_value(hash_locator, k).kvec_array, i);
+            kv_push(uint64_t, kh_value(hash_locator, k).vec, i);
         }
     }
 
     return hash_locator;
+}
+
+Clusters find_clusters(MinHashList& min_hash_list,
+                       HashLocator& hash_locator,
+                       const uint64_t limit)
+{
+    Clusters clusters(min_hash_list.size());
+
+    int ret;
+    khiter_t k;
+
+    for (uint64_t i = 0; i < min_hash_list.size(); i++)
+    {
+        khash_t(u64) *mutual = kh_init(u64);
+
+        for (auto hash : min_hash_list[i])
+        {
+            k = kh_get(kvector, hash_locator, hash);
+            Kvector indices = kh_value(hash_locator, k);
+
+            for (uint64_t j = 0; j < kv_size(indices.vec); j++)
+            {
+
+            }
+        }
+    }
+
+    return clusters;
 }
 
 int main(int argc, char** argv)
@@ -107,56 +174,35 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    std::fstream fin(argv[1], std::ios::in);
-
-    std::string fname;
-    std::getline(fin, fname);
-
-    std::vector<uint64_t*> sketches;
+    SketchFileNames filenames;
     {
-        Sketch sketch(fname.c_str());
-        k = sketch.k;
-        c = sketch.c;
-        s = sketch.s;
-
-        sketches.push_back(sketch.min_hash);
-
-        if (!validate)
-        {
-            while (std::getline(fin, fname))
-            {
-                sketch = Sketch(fname.c_str());
-                sketches.push_back(sketch.min_hash);
-            }
-        }
-        else
-        {
-            while (std::getline(fin, fname))
-            {
-                sketch = Sketch(fname.c_str());
-
-                if (k != sketch.k)
-                {
-                    std::cerr << "mismatched kmer size\n";
-                    exit(1);
-                }
-
-                if (c != sketch.c)
-                {
-                    std::cerr << "mismatched candiate limit\n";
-                    exit(1);
-                }
-
-                if (s != sketch.s)
-                {
-                    std::cerr << "mismatched min hash size\n";
-                    exit(1);
-                }
-
-                sketches.push_back(sketch.min_hash);
-            }
-        }
+        std::fstream fs(argv[1], std::ios::in);
+        std::istream_iterator<std::string> start(fs), end;
+        filenames.assign(start, end);
     }
 
-    auto hash_locator = make_hash_locator(sketches);
+    MinHashList min_hash_list;
+    min_hash_list.reserve(filenames.size());
+
+    for (auto& filename : filenames)
+    {
+        std::fstream fs(filename, std::ios::in);
+        std::istream_iterator<uint64_t> start(fs), end;
+
+        // skip k, c, s.
+        ++start;
+        ++start;
+        ++start;
+
+        min_hash_list.push_back({start, end});
+    }
+
+    for (auto& min_hash : min_hash_list)
+    {
+        for (auto hash : min_hash)
+        {
+            std::cout << hash << "\n";
+        }
+        std::cout << "\n";
+    }
 }
